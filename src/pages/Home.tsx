716cb5798c4249e43e6f421c84b0a61eaccc1653
vininribeiro23 <vininribeiro@hotmail.com>
2021-07-01 00:40:41 -0300

    contexto no react

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,32 @@
+
+import {useHistory} from 'react-router-dom'
+import { useContext } from 'react'
+
+import { AuthContext } from '../App'
+
+
 import illustrationImg from '../assets/imags/illustration.svg'
 import logoImg  from '../assets/imags/logo.svg'
 import googleIconImg from '../assets/imags/google-icon.svg'
 
 import { Button } from '../components/Button';
 
+
+
 import '../styles/auth.scss';
 
 export function Home(){
+    const history = useHistory();
+    const {user, signInWithGoogle} = useContext(AuthContext)
+     
+
+    async function handleCreateRoom(){
+
+        if(!user){
+            await signInWithGoogle
+        }
+        history.push('/rooms/new')      
+    }
     return(
         <div id="page-auth">
             <aside>
@@ -15,9 +35,10 @@ export function Home(){
                 <p>Tire as dúvidas da sua audiência em tempo-real</p>
             </aside>
             <main>
+                
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                     <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,44 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../App';
+
+
+import illustrationImg from '../assets/imags/illustration.svg'
+import logoImg  from '../assets/imags/logo.svg'
+
+
+import { Button } from '../components/Button';
+
+
+import '../styles/auth.scss';
+
+export function NewRoom(){
+
+    const {user} = useContext(AuthContext);
+    return(
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustracçao simolizando perguntas e respostas " />
+                <strong> Crie salas Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiência em tempo-real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <h1>{user?.name}</h1>
+                    <h2>Criar uma nova sala </h2>
+                    <form>
+                        <input 
+                        type="text"
+                        placeholder="Nome da sala "
+                         />
+                         <Button type="submit">
+                             Criar sala 
+                         </Button>
+                    </form>
+                    <p>Quer entrar em uma sala já existente? <Link to="/" >Clique aqui</Link></p>
+                </div>
+            </main>
+        </div>
+    )
+}
