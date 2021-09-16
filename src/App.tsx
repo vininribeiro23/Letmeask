59af1315dfaf22d17afdb4de976d043566ccd9ab
vininribeiro23 <vininribeiro@hotmail.com>
2021-09-15 23:45:43 -0300

    criando sala no firebase

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import {Room} from "./pages/Room";
 
 import { AuthContextProvider} from './contexts/AuthContext'
 
@@ -14,7 +15,9 @@ function App() {
     <BrowserRouter>
      <AuthContextProvider>
         <Route  path="/" exact component={Home} />
-        <Route  path="/rooms/new" component={NewRoom} />  
+        <Route  path="/rooms/new" exact component={NewRoom} />
+        <Route path="/rooms/:id" component={Room} />  
+        
       </AuthContextProvider>  
     </BrowserRouter>
   
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,16 +1,35 @@
 import { Link } from 'react-router-dom';
+import {FormEvent, useState} from 'react'
 
 import illustrationImg from '../assets/imags/illustration.svg'
 import logoImg  from '../assets/imags/logo.svg'
 
 
 import { Button } from '../components/Button';
-//import { useAuth } from '../hooks/useAuth';
+import { useAuth } from '../hooks/useAuth';
 
+import { database } from '../services/firebase';
 
 import '../styles/auth.scss';
 
 export function NewRoom(){
+
+    const [ newRoom, setNewRoom] = useState('');
+    const { user } = useAuth()
+
+    async function handleCreateRoom(event: FormEvent){
+        event.preventDefault();
+
+        if(newRoom.trim() === ''){
+            return;
+        }
+        const roomRef = database.ref('rooms')
+
+        const firebaseRoom = await roomRef.push({
+            title:newRoom,
+            authorId:user?.id,
+        }) 
+    }
    // const {user} = useAuth()
 
     return(
@@ -24,10 +43,12 @@ export function NewRoom(){
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                     <h2>Criar uma nova sala </h2>
-                    <form>
+                    <form onSubmit={handleCreateRoom}>
                         <input 
                         type="text"
                         placeholder="Nome da sala "
+                        onChange={event => setNewRoom(event.target.value) }
+                        value={newRoom}
                          />
                          <Button type="submit">
                              Criar sala 
